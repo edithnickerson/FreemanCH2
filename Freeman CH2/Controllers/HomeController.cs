384c6fe3d69cd3c9505a4ae8bf4c87f6b82a4208
edithnickerson <edithnickerson@gmail.com>
2018-02-04 23:53:00 -0600

    first commit

--- Freeman CH2/Controllers/HomeController.cs
+++ Freeman CH2/Controllers/HomeController.cs
@@ -4,15 +4,21 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using FreemanCH2.Models;
 using Freeman_CH2.Models;
 
-namespace Freeman_CH2.Controllers
+namespace FreemanCH2.Controllers
 {
     public class HomeController : Controller
     {
         public IActionResult Index()
         {
-            return View();
+
+            int hour = DateTime.Now.Hour;
+
+            ViewData["Greeting"] = hour < 12 ? "Good Morning" : "Good Afternoon";
+
+            return View("MyView");
         }
 
         public IActionResult About()
@@ -27,6 +33,44 @@ namespace Freeman_CH2.Controllers
             ViewData["Message"] = "Your contact page.";
 
             return View();
+        }
+
+        public IActionResult MyView()
+        {
+            return View();
+        }
+
+        //HTTP GET
+        [HttpGet]
+        public IActionResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RsvpForm(GuestResponse guestResponse)
+        {
+            //TODO: Process the guest response
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                //there was a problem
+                return View();
+            }
+        }
+
+        public IActionResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+        }
+
+        private IActionResult View(object p)
+        {
+            throw new NotImplementedException();
         }
 
         public IActionResult Error()
---
+++ Freeman CH2/Models/Repository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreemanCH2.Models
+{
+    public class Repository
+    {
+        public static IEnumerable<object> Responses { get; internal set; }
+
+        internal static void AddResponse(GuestResponse guestResponse)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
